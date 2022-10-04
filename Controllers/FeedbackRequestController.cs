73a7b5fa350fa323aadb1aa4bfe0d84ff1278369
Mark De Guzman <markdeguzman1142@gmail.com>
2022-10-04 00:15:51 -0700

    finished constructing layout for the Resolutions Page and the Feedback Routes

--- Controllers/FeedbackRequestController.cs
+++ Controllers/FeedbackRequestController.cs
@@ -5,16 +5,20 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using ResolutionManagement.Data;
+using ResolutionManagement.Models;
 
 namespace ResolutionManagement.Controllers
 {
     [Route("[controller]")]
     public class FeedbackRequestController : Controller
     {
+        private ApplicationDbContext _db;
         private readonly ILogger<FeedbackRequestController> _logger;
 
-        public FeedbackRequestController(ILogger<FeedbackRequestController> logger)
+        public FeedbackRequestController(ILogger<FeedbackRequestController> logger, ApplicationDbContext db)
         {
+            _db = db;
             _logger = logger;
         }
         [Route("")]
@@ -23,7 +27,35 @@ namespace ResolutionManagement.Controllers
         {
             return View();
         }
+        
+        [Route("Redirect/{ResolutionId}")]
+        public IActionResult FeedbackRedirection(int ResolutionId)
+        {
+            Console.Write("Hit the Feedback Redirection Route");
+            FeedbackRequest[] feedbackRequests = (from FeedbackRequest in _db.FeedbackRequests select FeedbackRequest).ToArray();
+            foreach (FeedbackRequest feedbackRequest in feedbackRequests)
+            {
+                if (feedbackRequest.ResolutionId == ResolutionId) {
+                    return RedirectToAction("FeedbackRequest", new { feedbackRequest.FeedbackRequestId });
+                }
+            }
+            return RedirectToAction("Error", "FeedbackRequestController");
+        }
 
+        [Route("FeedbackRequest/{FeedbackRequestId}")]
+        public IActionResult FeedbackRequest(int FeedbackRequestId)
+        {   
+            Console.Write("Hit the Feedback Request Route");
+            FeedbackRequest feedback = _db.FeedbackRequests.Find(FeedbackRequestId);
+            ViewData["Feedback"] = feedback;
+            return View();
+        }
+
+        [Route("Error")]
+        public IActionResult Error()
+        {
+            return View();
+        }
         // [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         // public IActionResult Error()
         // {
